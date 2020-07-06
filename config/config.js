6651a58efcd617e3eab65f0fde51d808c990f370
doodo1118 <doodo1118@gmail.com>
2020-07-06 17:14:18 +0900

    sequelize config modified for rds connection

--- config/config.js
+++ config/config.js
@@ -2,18 +2,20 @@ require('dotenv').config();
 
 module.exports = {
     development: {
-        username: 'root', 
+        username: 'admin', 
         password: process.env.SEQUELIZE_PASSWORD, 
         database: 'workout',
-        host: '127.0.0.1', 
+        // host: '127.0.0.1', 
+        host: 'workout.ckfzae75qkbl.ap-northeast-2.rds.amazonaws.com',
         dialect: 'mysql',
         operatorAliases: 'false', 
     }, 
     production: {
-        username: 'root', 
+        username: 'admin', 
         password: process.env.SEQUELIZE_PASSWORD,
         database: 'workout', 
-        host: '127.0.0.1', 
+        // host: '127.0.0.1', 
+        host: 'workout.ckfzae75qkbl.ap-northeast-2.rds.amazonaws.com',
         dialect: 'mysql', 
         operatorAliases: 'false', 
         logging : false,
